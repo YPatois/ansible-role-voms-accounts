bba5a9c981717d93a75814ef734e6a92165e5cfa
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-09-27 17:10:58 +0200

    Seems to work fine

--- vars/create_user_list.py
+++ vars/create_user_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.9
 # -*- coding: utf-8 -*-
 
 # FIXME: this should be dynamically generated inside ansible
@@ -11,37 +11,66 @@ import json
 import argparse
 
 class GridUser:
-    def __init__(self):
-        self.name = ''
-        self.uid = ''
-        self.gid = ''
-        self.roles = []
-
-    def __init__(self, name, uid, gid, roles):
+    def __init__(self, name, uid, gid, groups=None):
         self.name = name
         self.uid = uid
         self.gid = gid
-        self.roles = roles
+        self.groups = groups
 
-def generate_pool_users(name, base_uid, gid, roles):
-    return
+    def __str__(self):
+        # Return the user as a line
+        return self.name + ' ' + str(self.uid) + ' ' + str(self.gid)
+    
+    def to_yaml(self):
+        if (self.groups == None):
+            return {'name': self.name, 'uid': self.uid, 'gid': self.gid}
+        return {'name': self.name, 'uid': self.uid, 'gid': self.gid, 'groups': self.groups}
+
+def to3digits(n):
+    s = str(n)
+    if len(s) == 1:
+        return '00' + s
+    elif len(s) == 2:
+        return '0' + s
+    else:
+        return s
 
 
 class OneRole:
     def __init__(self, ydata):
         self.name = ydata['name']
         self.fqan = ydata['fqan']
-        self.gid = ydata['gid']
-
+        self.gid = int(ydata['gid'])
     def __str__(self):
         # Return the role as a line
         return self.name + ' ' + self.fqan + ' ' + str(self.gid)
 
-class OneVo:
+    def generate_user_list(self,vo):
+        # Generate a list of users
+        user_list = []
+        if (vo.gid == self.gid): # Main group
+            for i in range(200):
+                uid = vo.gid + i
+                name = vo.name + to3digits(i)
+                roles = [self.name]
+                user_list.append(GridUser(name, uid, self.gid))
+        else:
+            base_uid=vo.gid+500*(self.gid-vo.gid)
+            for i in range(21):
+                uid = base_uid + i
+                if (i==0):
+                    name = vo.name+ '_' + self.name
+                else:
+                    name = vo.name + '_' + self.name + to3digits(i)
+                user_list.append(GridUser(name, uid, self.gid, vo.gid))
+        return user_list
+        
+
+class OneVo(OneRole):
     def __init__(self, ydata):
         self.name = ydata['name']
         self.fqan = ydata['fqan']
-        self.gid = ydata['gid']
+        self.gid = int(ydata['gid'])
         # first check if there are roles
         self.roles = []
         if 'roles' in ydata:
@@ -49,6 +78,14 @@ class OneVo:
             for one_role in roles:
                 self.roles.append(OneRole(one_role))
     
+    def generate_user_list(self):
+        # Generate a list of users
+        user_list = []
+        user_list = user_list + super().generate_user_list(self)
+        for one_role in self.roles:
+            user_list = user_list + one_role.generate_user_list(self)
+        return user_list
+
     def __str__(self):
         # Return the VO as a line for each role
         s=self.name + ' ' + self.fqan + ' ' + str(self.gid)
@@ -59,14 +96,25 @@ class OneVo:
 def main():
     # Parsing supported_vos.yaml
     data = yaml.safe_load(open('supported_vos.yaml', 'r'))
+    #print (data)
+    #return
     vo_list = []
     for one_vo in data['vo_details']:
         vo_list.append(OneVo(one_vo))
     
-    # Write the list
-    with open('user_list.txt', 'w') as f:
-        for one_vo in vo_list:
-            f.write(str(one_vo) + '\n')
+    # Generate the user list
+    user_list = []
+    for vo in vo_list:
+        user_list = user_list + vo.generate_user_list()
+    
+    # convert user list to yaml
+    user_list_yaml = [user.to_yaml() for user in user_list]
+
+    # Final dictionary
+    grid_users = {'grid_users': user_list_yaml}
+    # Write the user list in yaml
+    with open('user_list.yaml', 'w') as f:
+        yaml.safe_dump(grid_users, f, default_flow_style=False, sort_keys=False)
 
 
 if __name__ == "__main__":
